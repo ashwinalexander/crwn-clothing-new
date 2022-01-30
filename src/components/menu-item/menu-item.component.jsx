f0b900c6c0c8754c604896a7cb7af453e5124254
ashwin alexander <alexwin75@gmail.com>
2022-01-30 10:19:36 -0500

    Adding in routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
+//higher order component = takes a component as an argument and returns a modified component
+import { withRouter } from 'react-router-dom';
+
 //functional since we do not need our component to hold any state
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => {
+      history.push(`${match.url}${linkUrl}`);
+    }}
+  >
     <div
       className='background-image'
       style={{
@@ -17,4 +25,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+//higher order component = takes a component as an argument and returns a modified component
+export default withRouter(MenuItem);
