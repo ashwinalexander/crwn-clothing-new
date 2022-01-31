1f0a36f96d1c3e283fb3cf7bc3eb22df7e9f9130
ashwin alexander <alexwin75@gmail.com>
2022-01-30 20:15:20 -0500

    adding in the Sign Up component and reusable form components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
         <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
