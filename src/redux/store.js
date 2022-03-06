1902810ddf3012cc5a720304a7c53a41c435c7ef
ashwin alexander <alexwin75@gmail.com>
2022-03-05 19:42:10 -0500

    optimising production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 //before using Redux Persist
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
